703392543f4f255792c58f01852cd86525e19eb4
kaya <kaya@treenod.com>
2021-04-10 16:02:17 +0900

    index 페이지 설정

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask, escape, request
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/api/hello")
+    @app.route("/hello")
     def hello():
         name = request.args.get("name", "World")
         return f"Hello, {escape(name)}!"
