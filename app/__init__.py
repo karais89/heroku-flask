7972ce668d6ec696d66b485e3add3a7f1aa49df2
kaya <kaya@treenod.com>
2021-04-10 13:21:52 +0900

    변경

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask, escape, request
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/hello")
+    @app.route("/api/hello")
     def hello():
         name = request.args.get("name", "World")
         return f"Hello, {escape(name)}!"
